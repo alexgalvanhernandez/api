165f13ec0b9c0a9237d3dd84b64b462a7cee3590
Alejandro Galvan <alexgalvanhenandez@gmail.com>
2015-05-22 10:14:35 -0500

    app

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "mysql.hostinger.mx");
+define("DB_USER", "u953042257_allen");
+define("DB_PASSWORD", "20034747gh12");
+define("DB_DATABASE", "u953042257_users");
+?>
